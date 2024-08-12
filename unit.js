fd242acfb43e9ae2f9645a00e7f1d94553e98d59
fretzestavillo <estavillofretz@gmail.com>
2024-08-12 14:29:15 +0800

    add JSDOM for scraping HTML body

---
+++ unit.js
@@ -0,0 +1,32 @@
+import { JSDOM } from "jsdom";
+
+export function normalizeURL(url) {
+  const urlObj = new URL(url);
+  let fullPath = `${urlObj.host}${urlObj.pathname}`;
+  if (fullPath.slice(-1) === "/") {
+    fullPath = fullPath.slice(0, -1);
+  }
+  return fullPath;
+}
+
+export function getURLsFromHTML(html, baseURL) {
+  const urls = [];
+  const dom = new JSDOM(html);
+  const anchors = dom.window.document.querySelectorAll("a");
+
+  for (const anchor of anchors) {
+    if (anchor.hasAttribute("href")) {
+      let href = anchor.getAttribute("href");
+
+      try {
+        // convert any relative URLs to absolute URLs
+        href = new URL(href, baseURL).href;
+        urls.push(href);
+      } catch (err) {
+        console.log(`${err.message}: ${href}`);
+      }
+    }
+  }
+
+  return urls;
+}
